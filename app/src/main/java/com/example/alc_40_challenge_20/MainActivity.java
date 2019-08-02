15e36e700a2a8e92eb2eb0a073a8deb6f0994b07
tony-gabriel <tonyganriel@gmail.com>
2019-08-02 05:12:14 +0100

    Set up firebase with errors and mainactivity.java

--- app/src/main/java/com/example/alc_40_challenge_20/MainActivity.java
+++ app/src/main/java/com/example/alc_40_challenge_20/MainActivity.java
@@ -1,16 +1,44 @@
 package com.example.alc_40_challenge_20;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText email, name, password;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         email = findViewById(R.id.email);
+         name = findViewById(R.id.name);
+         password = findViewById(R.id.password);
+         save = findViewById(R.id.btn_save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                register();
+            }
+        });
 
 
         setTitle("Sign up");
     }
+
+    private void register(){
+
+
+
+    }
+
+
 }
+
+
